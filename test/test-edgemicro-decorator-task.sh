c947f308afa438e6d6f527dae5628d440c60e870
Sean Williams <sean.williams.11@gmail.com>
2017-03-08 15:55:26 -0600

    updated pipeline to call .sh file directly, removed bundle exec from .sh

--- test/test-edgemicro-decorator-task.sh
+++ test/test-edgemicro-decorator-task.sh
@@ -14,7 +14,6 @@ set -e -x
 
 #pushd edgemicro-decorator
   # test the yaml file code
+  echo "Executing cf cups..."
   cf cups edgemicro_service -p '{"application_name":"edgemicro_service", "org":"demo", "env":"test", "user":"sw@email.com","pass":"password", "edgemicro_version":"2.3.1", "edgemicro_port":"8080", "onpremises": "true", "onprem_config" : {"runtime_url": "http://192.168.56.101:9001", "mgmt_url" : "http://192.168.56.101:8080", "virtual_host" : "default"}, "yaml_included":"true","yaml_name":"demo-test-config.yaml", "tags": ["edgemicro"]}'
-  bundle install
-  bundle exec rspec
 #popd
