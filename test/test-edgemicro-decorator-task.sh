25adc91da7267c37c3c7255e77a4a38294047bcf
Sean Williams <sean.williams.11@gmail.com>
2017-03-08 16:19:37 -0600

    testing git clone in shell script.

--- test/test-edgemicro-decorator-task.sh
+++ test/test-edgemicro-decorator-task.sh
@@ -1,19 +1,11 @@
 #!/bin/bash
 
-set -e -x
+set -e
+set -x
 
-
+echo "test"
 # set the target
 #sudo route add -net 10.244.0.0/19 192.168.50.4
-
-# upload the decorator
-
-# restage the application
-
-# execute the test scripts
-
-#pushd edgemicro-decorator
-  # test the yaml file code
-  echo "Executing cf cups..."
+git clone https://github.com/swilliams11/edgemicro-decorator.git
+echo "Executing cf cups..."
   #cf cups edgemicro_service -p '{"application_name":"edgemicro_service", "org":"demo", "env":"test", "user":"sw@email.com","pass":"password", "edgemicro_version":"2.3.1", "edgemicro_port":"8080", "onpremises": "true", "onprem_config" : {"runtime_url": "http://192.168.56.101:9001", "mgmt_url" : "http://192.168.56.101:8080", "virtual_host" : "default"}, "yaml_included":"true","yaml_name":"demo-test-config.yaml", "tags": ["edgemicro"]}'
-#popd
