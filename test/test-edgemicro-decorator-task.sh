6ea224e9a4e40cb669c21d3f5b71708de3e25c01
Sean Williams <sean.williams.11@gmail.com>
2017-03-08 18:28:42 -0600

    added ip to bosh

--- test/test-edgemicro-decorator-task.sh
+++ test/test-edgemicro-decorator-task.sh
@@ -11,7 +11,8 @@ ls -la resource-edgemicro-decorator/test/
 pwd
 dpkg -i resource-edgemicro-decorator/test/cf-cli-installer_6.25.0_x86-64.deb && apt-get install -f
 #ip route add 10.244.0.0/19 via 192.168.50.4 dev eth0
-cf api --skip-ssl-validation https://api.bosh-lite.com
+#cf api --skip-ssl-validation https://api.bosh-lite.com
+cf api --skip-ssl-validation http://192.168.50.4 
 cf login --skip-ssl-validation -a https://api.bosh-lite.com -u admin
 echo "Executing cf cups..."
 cf cups edgemicro_service -p '{"application_name":"edgemicro_service", "org":"demo", "env":"test", "user":"sw@email.com","pass":"password", "edgemicro_version":"2.3.1", "edgemicro_port":"8080", "onpremises": "true", "onprem_config" : {"runtime_url": "http://192.168.56.101:9001", "mgmt_url" : "http://192.168.56.101:8080", "virtual_host" : "default"}, "yaml_included":"true","yaml_name":"demo-test-config.yaml", "tags": ["edgemicro"]}'
