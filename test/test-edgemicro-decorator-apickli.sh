0e40f9790e81e9688a93770cb2c07bba50709aaa
Sean Williams <sean.williams.11@gmail.com>
2017-04-07 15:56:47 -0500

    fixes #13; added test8 to confirm node.js download is working. update other tests to include the nodejs_version: node-v6.9.1-linux-x64.tar.xz property in the service instance. This field is now required.

--- test/test-edgemicro-decorator-apickli.sh
+++ test/test-edgemicro-decorator-apickli.sh
@@ -40,10 +40,18 @@ echo "update the security groups"
 #cf update-security-group user_bosh_deployments ../setup/cf_securitygroups/user_bosh_deployments.json
 
 #make sure we have a clean environment before we test.
-cf delete spring_hellotest -f
-cf delete-service edgemicro_service -f
+if [[ `cf app spring_hellotest` != *"FAILED"* ]]; then
+  echo "spring_hellotest does not exists!"
+    cf unbind-service spring_hellotest edgemicro_service
+    cf delete spring_hellotest -f
+fi
+
+if [[ `cf service edgemicro_service` != *"FAILED"* ]]; then
+  cf delete-service edgemicro_service -f
+fi
+
 #create the default instance so that it exists, and subsequents updates don't fail
-cf cups edgemicro_service -p "{\"application_name\":\"edgemicro_service\", \"org\":\"$org\", \"env\":\"$env\", \"user\":\"$username\",\"pass\":\"$password\", \"edgemicro_version\":\"$edgemicro_version\", \"edgemicro_port\":\"$edgemicro_port\", \"onpremises\": \"true\", \"onprem_config\" : {\"runtime_url\": \"$edge_runtimeurl\", \"mgmt_url\" : \"$edge_mgmturl\", \"virtual_host\" : \"default\"}, \"tags\": [\"edgemicro\"]}"
+cf cups edgemicro_service -p "{\"application_name\":\"edgemicro_service\", \"nodejs_version\": \"node-v6.9.1-linux-x64.tar.xz\", \"org\":\"$org\", \"env\":\"$env\", \"user\":\"$username\",\"pass\":\"$password\", \"edgemicro_version\":\"$edgemicro_version\", \"edgemicro_port\":\"$edgemicro_port\", \"onpremises\": \"true\", \"onprem_config\" : {\"runtime_url\": \"$edge_runtimeurl\", \"mgmt_url\" : \"$edge_mgmturl\", \"virtual_host\" : \"default\"}, \"tags\": [\"edgemicro\"]}"
 pushd gs-rest-service/complete
   cf push --no-start
   cf enable-diego spring_hellotest
@@ -52,7 +60,7 @@ popd
 
 if [ "$TEST" == "all" ] || [ "$TEST" == "test1" ]; then
   echo "******** 1 Test - Default Configuration *********"
-  cf uups edgemicro_service -p "{\"application_name\":\"edgemicro_service\", \"org\":\"$org\", \"env\":\"$env\", \"user\":\"$username\",\"pass\":\"$password\", \"edgemicro_version\":\"$edgemicro_version\", \"edgemicro_port\":\"$edgemicro_port\", \"onpremises\": \"true\", \"onprem_config\" : {\"runtime_url\": \"$edge_runtimeurl\", \"mgmt_url\" : \"$edge_mgmturl\", \"virtual_host\" : \"default\"}, \"tags\": [\"edgemicro\"]}"
+  cf uups edgemicro_service -p "{\"application_name\":\"edgemicro_service\", \"nodejs_version\": \"node-v6.9.1-linux-x64.tar.xz\", \"org\":\"$org\", \"env\":\"$env\", \"user\":\"$username\",\"pass\":\"$password\", \"edgemicro_version\":\"$edgemicro_version\", \"edgemicro_port\":\"$edgemicro_port\", \"onpremises\": \"true\", \"onprem_config\" : {\"runtime_url\": \"$edge_runtimeurl\", \"mgmt_url\" : \"$edge_mgmturl\", \"virtual_host\" : \"default\"}, \"tags\": [\"edgemicro\"]}"
   #pushd gs-rest-service/complete
     #sed -ie 's/name: gs-rest-service/name: spring_hello/' manifest.yml
     #sed -ie 's/guides.spring.io/bosh-lite.com/' manifest.yml
@@ -65,7 +73,7 @@ fi
 
 if [ "$TEST" == "all" ] || [ "$TEST" == "test2" ]; then
   echo "******** 2 Test - Spike Arrest *********"
-  cf uups edgemicro_service -p "{\"application_name\":\"edgemicro_service\", \"org\":\"$org\", \"env\":\"$env\", \"user\":\"$username\",\"pass\":\"$password\", \"edgemicro_version\":\"$edgemicro_version\", \"edgemicro_port\":\"$edgemicro_port\", \"onpremises\": \"true\", \"onprem_config\" : {\"runtime_url\": \"$edge_runtimeurl\", \"mgmt_url\" : \"$edge_mgmturl\", \"virtual_host\" : \"default\"}, \"enable_spike_arrest\": \"true\", \"spike_arrest_config\" : {\"timeunit\": \"minute\", \"allow\" : \"$SPIKE_ARREST_RATE\"}, \"tags\": [\"edgemicro\"]}"
+  cf uups edgemicro_service -p "{\"application_name\":\"edgemicro_service\", \"nodejs_version\": \"node-v6.9.1-linux-x64.tar.xz\", \"org\":\"$org\", \"env\":\"$env\", \"user\":\"$username\",\"pass\":\"$password\", \"edgemicro_version\":\"$edgemicro_version\", \"edgemicro_port\":\"$edgemicro_port\", \"onpremises\": \"true\", \"onprem_config\" : {\"runtime_url\": \"$edge_runtimeurl\", \"mgmt_url\" : \"$edge_mgmturl\", \"virtual_host\" : \"default\"}, \"enable_spike_arrest\": \"true\", \"spike_arrest_config\" : {\"timeunit\": \"minute\", \"allow\" : \"$SPIKE_ARREST_RATE\"}, \"tags\": [\"edgemicro\"]}"
 
   #cf restage spring_hellotest
   pushd gs-rest-service/complete
@@ -77,7 +85,7 @@ fi
 
 if [ "$TEST" == "all" ] || [ "$TEST" == "test3" ]; then
   echo "******** 3 Test - enable custom plugins *********"
-  cf uups edgemicro_service -p "{\"application_name\":\"edgemicro_service\", \"org\":\"$org\", \"env\":\"$env\", \"user\":\"$username\",\"pass\":\"$password\", \"edgemicro_version\":\"$edgemicro_version\", \"edgemicro_port\":\"$edgemicro_port\", \"onpremises\": \"true\", \"onprem_config\" : {\"runtime_url\": \"$edge_runtimeurl\", \"mgmt_url\" : \"$edge_mgmturl\", \"virtual_host\" : \"default\"}, \"enable_custom_plugins\":\"true\", \"plugins\":\"oauth,plugin1,plugin2\", \"yaml_included\":\"false\",\"tags\": [\"edgemicro\"]}"
+  cf uups edgemicro_service -p "{\"application_name\":\"edgemicro_service\", \"nodejs_version\": \"node-v6.9.1-linux-x64.tar.xz\", \"org\":\"$org\", \"env\":\"$env\", \"user\":\"$username\",\"pass\":\"$password\", \"edgemicro_version\":\"$edgemicro_version\", \"edgemicro_port\":\"$edgemicro_port\", \"onpremises\": \"true\", \"onprem_config\" : {\"runtime_url\": \"$edge_runtimeurl\", \"mgmt_url\" : \"$edge_mgmturl\", \"virtual_host\" : \"default\"}, \"enable_custom_plugins\":\"true\", \"plugins\":\"oauth,plugin1,plugin2\", \"yaml_included\":\"false\",\"tags\": [\"edgemicro\"]}"
   #make sure plugin1 and plugin2 are uploaded with the decorator before testing
   pushd ../
     ./upload
@@ -92,7 +100,7 @@ fi
 
 if [ "$TEST" == "all" ] || [ "$TEST" == "test4" ]; then
   echo "******** 4 Test - enable custom plugins and spike arrest *********"
-  cf uups edgemicro_service -p "{\"application_name\":\"edgemicro_service\", \"org\":\"$org\", \"env\":\"$env\", \"user\":\"$username\",\"pass\":\"$password\", \"edgemicro_version\":\"$edgemicro_version\", \"edgemicro_port\":\"$edgemicro_port\", \"onpremises\": \"true\", \"onprem_config\" : {\"runtime_url\": \"$edge_runtimeurl\", \"mgmt_url\" : \"$edge_mgmturl\", \"virtual_host\" : \"default\"}, \"enable_custom_plugins\":\"true\", \"plugins\":\"oauth,spikearrest,plugin1,plugin2\", \"spike_arrest_config\" : {\"timeunit\": \"minute\", \"allow\" : \"$SPIKE_ARREST_RATE\"}, \"tags\": [\"edgemicro\"]}"
+  cf uups edgemicro_service -p "{\"application_name\":\"edgemicro_service\", \"nodejs_version\": \"node-v6.9.1-linux-x64.tar.xz\", \"org\":\"$org\", \"env\":\"$env\", \"user\":\"$username\",\"pass\":\"$password\", \"edgemicro_version\":\"$edgemicro_version\", \"edgemicro_port\":\"$edgemicro_port\", \"onpremises\": \"true\", \"onprem_config\" : {\"runtime_url\": \"$edge_runtimeurl\", \"mgmt_url\" : \"$edge_mgmturl\", \"virtual_host\" : \"default\"}, \"enable_custom_plugins\":\"true\", \"plugins\":\"oauth,spikearrest,plugin1,plugin2\", \"spike_arrest_config\" : {\"timeunit\": \"minute\", \"allow\" : \"$SPIKE_ARREST_RATE\"}, \"tags\": [\"edgemicro\"]}"
   pushd gs-rest-service/complete
     cf push
   popd
@@ -105,7 +113,7 @@ fi
 
 if [ "$TEST" == "all" ] || [ "$TEST" == "test5" ]; then
   echo "******** 5 Test - include yaml *********"
-  cf uups edgemicro_service -p "{\"application_name\":\"edgemicro_service\", \"org\":\"$org\", \"env\":\"$env\", \"user\":\"$username\",\"pass\":\"$password\", \"edgemicro_version\":\"$edgemicro_version\", \"edgemicro_port\":\"$edgemicro_port\", \"onpremises\": \"true\", \"onprem_config\" : {\"runtime_url\": \"$edge_runtimeurl\", \"mgmt_url\" : \"$edge_mgmturl\", \"virtual_host\" : \"default\"}, \"yaml_included\":\"true\",\"yaml_name\":\"$YAML_FILE\", \"tags\": [\"edgemicro\"]}"
+  cf uups edgemicro_service -p "{\"application_name\":\"edgemicro_service\", \"nodejs_version\": \"node-v6.9.1-linux-x64.tar.xz\", \"org\":\"$org\", \"env\":\"$env\", \"user\":\"$username\",\"pass\":\"$password\", \"edgemicro_version\":\"$edgemicro_version\", \"edgemicro_port\":\"$edgemicro_port\", \"onpremises\": \"true\", \"onprem_config\" : {\"runtime_url\": \"$edge_runtimeurl\", \"mgmt_url\" : \"$edge_mgmturl\", \"virtual_host\" : \"default\"}, \"yaml_included\":\"true\",\"yaml_name\":\"$YAML_FILE\", \"tags\": [\"edgemicro\"]}"
   pushd gs-rest-service/complete
     cf push
   popd
@@ -115,7 +123,7 @@ fi
 
 if [ "$TEST" == "all" ] || [ "$TEST" == "test6" ]; then
   echo "******** 6 Test - Enable Quota *********"
-  cf uups edgemicro_service -p "{\"application_name\":\"edgemicro_service\", \"org\":\"$org\", \"env\":\"$env\", \"user\":\"$username\",\"pass\":\"$password\", \"edgemicro_version\":\"$edgemicro_version\", \"edgemicro_port\":\"$edgemicro_port\", \"onpremises\": \"true\", \"onprem_config\" : {\"runtime_url\": \"$edge_runtimeurl\", \"mgmt_url\" : \"$edge_mgmturl\", \"virtual_host\" : \"default\"}, \"enable_quota\":\"true\", \"tags\": [\"edgemicro\"]}"
+  cf uups edgemicro_service -p "{\"application_name\":\"edgemicro_service\", \"nodejs_version\": \"node-v6.9.1-linux-x64.tar.xz\", \"org\":\"$org\", \"env\":\"$env\", \"user\":\"$username\",\"pass\":\"$password\", \"edgemicro_version\":\"$edgemicro_version\", \"edgemicro_port\":\"$edgemicro_port\", \"onpremises\": \"true\", \"onprem_config\" : {\"runtime_url\": \"$edge_runtimeurl\", \"mgmt_url\" : \"$edge_mgmturl\", \"virtual_host\" : \"default\"}, \"enable_quota\":\"true\", \"tags\": [\"edgemicro\"]}"
   pushd gs-rest-service/complete
     cf push
   popd
@@ -125,13 +133,27 @@ fi
 
 if [ "$TEST" == "all" ] || [ "$TEST" == "test7" ]; then
   echo "******** 7 Test - Enable Spike Arrest and Quota *********"
-  cf uups edgemicro_service -p "{\"application_name\":\"edgemicro_service\", \"org\":\"$org\", \"env\":\"$env\", \"user\":\"$username\",\"pass\":\"$password\", \"edgemicro_version\":\"$edgemicro_version\", \"edgemicro_port\":\"$edgemicro_port\", \"onpremises\": \"true\", \"onprem_config\" : {\"runtime_url\": \"$edge_runtimeurl\", \"mgmt_url\" : \"$edge_mgmturl\", \"virtual_host\" : \"default\"}, \"enable_quota\":\"true\", \"enable_spike_arrest\": \"true\", \"spike_arrest_config\" : {\"timeunit\": \"minute\", \"allow\" : \"$SPIKE_ARREST_RATE\"}, \"tags\": [\"edgemicro\"]}"
+  cf uups edgemicro_service -p "{\"application_name\":\"edgemicro_service\", \"nodejs_version\": \"node-v6.9.1-linux-x64.tar.xz\", \"org\":\"$org\", \"env\":\"$env\", \"user\":\"$username\",\"pass\":\"$password\", \"edgemicro_version\":\"$edgemicro_version\", \"edgemicro_port\":\"$edgemicro_port\", \"onpremises\": \"true\", \"onprem_config\" : {\"runtime_url\": \"$edge_runtimeurl\", \"mgmt_url\" : \"$edge_mgmturl\", \"virtual_host\" : \"default\"}, \"enable_quota\":\"true\", \"enable_spike_arrest\": \"true\", \"spike_arrest_config\" : {\"timeunit\": \"minute\", \"allow\" : \"$SPIKE_ARREST_RATE\"}, \"tags\": [\"edgemicro\"]}"
   pushd gs-rest-service/complete
     cf push
   popd
   sleep 30
   cucumber-js features/edgemicroSpikeArrest.feature
-  cf uups edgemicro_service -p "{\"application_name\":\"edgemicro_service\", \"org\":\"$org\", \"env\":\"$env\", \"user\":\"$username\",\"pass\":\"$password\", \"edgemicro_version\":\"$edgemicro_version\", \"edgemicro_port\":\"$edgemicro_port\", \"onpremises\": \"true\", \"onprem_config\" : {\"runtime_url\": \"$edge_runtimeurl\", \"mgmt_url\" : \"$edge_mgmturl\", \"virtual_host\" : \"default\"}, \"enable_quota\":\"true\", \"enable_spike_arrest\": \"true\", \"spike_arrest_config\" : {\"timeunit\": \"second\", \"allow\" : \"500\"}, \"tags\": [\"edgemicro\"]}"
+  cf uups edgemicro_service -p "{\"application_name\":\"edgemicro_service\", \"nodejs_version\": \"node-v6.9.1-linux-x64.tar.xz\", \"org\":\"$org\", \"env\":\"$env\", \"user\":\"$username\",\"pass\":\"$password\", \"edgemicro_version\":\"$edgemicro_version\", \"edgemicro_port\":\"$edgemicro_port\", \"onpremises\": \"true\", \"onprem_config\" : {\"runtime_url\": \"$edge_runtimeurl\", \"mgmt_url\" : \"$edge_mgmturl\", \"virtual_host\" : \"default\"}, \"enable_quota\":\"true\", \"enable_spike_arrest\": \"true\", \"spike_arrest_config\" : {\"timeunit\": \"second\", \"allow\" : \"500\"}, \"tags\": [\"edgemicro\"]}"
+  cf restage spring_hellotest
+  sleep 30
+  cucumber-js features/edgemicroQuota.feature
+fi
+
+if [ "$TEST" == "all" ] || [ "$TEST" == "test8" ]; then
+  echo "******** 8 Test - nodejs_version_number & Enable Spike Arrest & Quota*********"
+  cf uups edgemicro_service -p "{\"application_name\":\"edgemicro_service\", \"nodejs_version_number\": \"7.8.0\", \"org\":\"$org\", \"env\":\"$env\", \"user\":\"$username\",\"pass\":\"$password\", \"edgemicro_version\":\"$edgemicro_version\", \"edgemicro_port\":\"$edgemicro_port\", \"onpremises\": \"true\", \"onprem_config\" : {\"runtime_url\": \"$edge_runtimeurl\", \"mgmt_url\" : \"$edge_mgmturl\", \"virtual_host\" : \"default\"}, \"enable_quota\":\"true\", \"enable_spike_arrest\": \"true\", \"spike_arrest_config\" : {\"timeunit\": \"minute\", \"allow\" : \"$SPIKE_ARREST_RATE\"}, \"tags\": [\"edgemicro\"]}"
+  pushd gs-rest-service/complete
+    cf push
+  popd
+  sleep 30
+  cucumber-js features/edgemicroSpikeArrest.feature
+  cf uups edgemicro_service -p "{\"application_name\":\"edgemicro_service\", \"nodejs_version_number\": \"7.8.0\", \"org\":\"$org\", \"env\":\"$env\", \"user\":\"$username\",\"pass\":\"$password\", \"edgemicro_version\":\"$edgemicro_version\", \"edgemicro_port\":\"$edgemicro_port\", \"onpremises\": \"true\", \"onprem_config\" : {\"runtime_url\": \"$edge_runtimeurl\", \"mgmt_url\" : \"$edge_mgmturl\", \"virtual_host\" : \"default\"}, \"enable_quota\":\"true\", \"enable_spike_arrest\": \"true\", \"spike_arrest_config\" : {\"timeunit\": \"second\", \"allow\" : \"500\"}, \"tags\": [\"edgemicro\"]}"
   cf restage spring_hellotest
   sleep 30
   cucumber-js features/edgemicroQuota.feature
